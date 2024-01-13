8be34f1fe91644496ec052dad38a3297189a2cf5
Alexis Calixto <alexiscfx1@gmail.com>
2024-01-13 11:23:11 -0300

    Age 'Formula'

--- main.js
+++ main.js
@@ -7,7 +7,20 @@ const year = date.getFullYear();
 const iDay = document.querySelector('#day')
 const iMonth = document.querySelector('#month')
 const iYear = document.querySelector('#year')
-const button = document.querySelector('.calculator__img')
+const button = document.querySelector('.calculator__img').addEventListener('click', () => { // Age formula
+    if (iDay.value != 0 & iMonth.value != 0 & iYear.value != 0) {
+        if (iDay.checkValidity() & iMonth.checkValidity() & iYear.checkValidity()) {
+            rDay = day - iDay.value
+            rMonth = month - iMonth.value + 12
+            rYear = year - iYear.value -1
+            result(rDay, rMonth, rYear)
+        } else {
+            console.log('Error, Los valores ingresados son invalidos');
+        }
+    } else {
+        console.log('Error, Los valores estan vacios');
+    }
+})
 
 // Conditions for date entries
 iMonth.addEventListener('input', () => {
